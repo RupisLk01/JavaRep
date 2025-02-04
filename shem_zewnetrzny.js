ad0967de47388fa7729c2723f33f36e2dc460ab9
RupisLK01 <shemetovskij123@gmail.com>
2025-02-04 17:55:32 +0100

    dodałem nowe
    a

--- shem_zewnetrzny.js
+++ shem_zewnetrzny.js
@@ -1,2 +1,3 @@
 document.write("ostatnia modyfikacja strony".fontcolor("lime").fontsize(5)+"<br>");
 document.write(document.lastModified.strike());
+<button class="button" onclick="window.location.href='index.html'"><Shemet-Zadania>wróc</Shemet-Zadania></button>
